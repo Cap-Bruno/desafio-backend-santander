722e3be3b382970208c20c9fa3fc60287ac26821
Bruno Campos <bruno.b.campos@capgemini.com>
2023-10-30 14:25:32 -0300

    add Swagger

--- santanderInternetBanking/src/main/java/com/santanderInternetBanking/SantanderInternetBankingApplication.java
+++ santanderInternetBanking/src/main/java/com/santanderInternetBanking/SantanderInternetBankingApplication.java
@@ -1,9 +1,12 @@
 package com.santanderInternetBanking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger API", version = "1", description = "Swagger Desafio Santander"))
 public class SantanderInternetBankingApplication {
 
 	public static void main(String[] args) {
