770114d9579876aff4860f17d7c50c8e5f54993e
Bruno Campos <bruno.b.campos@capgemini.com>
2023-10-30 14:25:58 -0300

    add data sql

---
+++ santanderInternetBanking/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Clients (id, name, exclusive, balance, num_account, birthdate) VALUES (DEFAULT, 'Bruno', 1, 2000, '0000', '2000-03-01');
+INSERT INTO Clients (id, name, exclusive, balance, num_account, birthdate) VALUES (DEFAULT, 'Pedro', 1, 100, '1111', '1999-02-20');
+INSERT INTO Clients (id, name, exclusive, balance, num_account, birthdate) VALUES (DEFAULT, 'Carlos', 1, 777, '2222', '2001-11-25');
+INSERT INTO Clients (id, name, exclusive, balance, num_account, birthdate) VALUES (DEFAULT, 'Joao', 1, 4550, '3333', '2003-06-12');
+INSERT INTO Clients (id, name, exclusive, balance, num_account, birthdate) VALUES (DEFAULT, 'Drake', 1, 200, '4444', '1995-08-06');
