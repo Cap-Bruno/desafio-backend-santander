58dadcbcc8fa1f33f51f9ed716f27c99f154e410
Bruno Campos <bruno.b.campos@capgemini.com>
2023-11-16 12:11:32 -0300

    add validation

--- santanderInternetBanking/src/main/java/com/santanderInternetBanking/dto/ClientDTO.java
+++ santanderInternetBanking/src/main/java/com/santanderInternetBanking/dto/ClientDTO.java
@@ -1,8 +1,19 @@
 package com.santanderInternetBanking.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
-public record ClientDTO(String name, boolean exclusive, BigDecimal balance, String numAccount, Date birthdate) {
+public record ClientDTO(@NotEmpty(message = "Campo Nome obrigatório")
+                        String name,
+                        boolean exclusive,
+                        @NotNull(message = "O Saldo é obrigatório")
+                        BigDecimal balance,
+                        @NotEmpty(message = "O Numero da Conta é obrigatório")
+                        String numAccount,
+                        @NotNull(message = "Data de aniversario obrigatória")
+                        Date birthdate) {
 
 }
